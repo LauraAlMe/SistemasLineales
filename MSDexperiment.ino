0fe5fd54bf42d5c48cb9a62d65f4b37bd3127c1d
Laura Alvarez <89430971+LauraAlMe@users.noreply.github.com>
2021-11-22 23:00:14 -0600

    Create MSDexperiment.ino

---
+++ MSDexperiment.ino
@@ -0,0 +1,21 @@
+// Declare variables
+String incomingString;
+float dato;
+float F,m=1,b=1,k=0.2;
+float x1=0,x2=0,T=0.0311;
+void setup(){
+  // Initialize serial port
+  Serial.begin(9600);
+  Serial.setTimeout(3);
+}
+
+void loop(){
+  if (Serial.available() > 0) {
+    incomingString = Serial.readString();
+    dato=incomingString.toFloat(); 
+    F=10*sin(dato); 
+    x1=x1+T*x2;
+    x2=x2+T*(-(k/m)*x1-(b/m)*x2+F/m);
+    Serial.println(x1);
+  }
+}
